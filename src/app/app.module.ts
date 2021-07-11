e708c9421b7b5fd49d511c0b7360dbd652576f4c
Andrew Beltrán <gchoandrew@gmail.com>
2021-07-11 09:44:35 +0100

    deleted unused elements

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,10 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { BoardElementComponent } from './components/board-element/board-element.component';
-import { FieldElementComponent } from './components/board-element/field-element/field-element.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BoardElementComponent,
-    FieldElementComponent
   ],
   imports: [
     BrowserModule
--- src/app/components/board-element/field-element/field-element.component.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  selector: 'field-element',
-  templateUrl: './field-element.component.html',
-  styleUrls: ['./field-element.component.scss']
-})
-export class FieldElementComponent {
-}
